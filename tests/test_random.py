06acc28f999dd097cfa2e11ab3f178bb2b7fb812
Amir Kadivar <amir@amirkdv.ca>
2016-07-01 12:31:57 -0400

    implement MutationProcess.noisy_reads()

--- tests/test_random.py
+++ tests/test_random.py
@@ -3,7 +3,7 @@ import pytest
 import mock
 
 from biseqt.sequence import Alphabet
-from biseqt.random import rand_seq, MutationProcess, rand_reads
+from biseqt.random import rand_seq, MutationProcess, rand_read
 from biseqt.random import np  # to mock
 
 
@@ -19,25 +19,44 @@ def test_lossless_reads():
     A = Alphabet('ACGT')
     S = rand_seq(A, 100)
     with pytest.raises(AssertionError):
-        next(rand_reads(S, len_mean=200, num=1))  # len_mean must be < len(S)
-    with pytest.raises(AssertionError):
-        # at least one of num or expected_coverage given
-        next(rand_reads(S, len_mean=50))
+        next(rand_read(S, len_mean=200, num=1))  # len_mean must be < len(S)
     with pytest.raises(AssertionError):
         # at most one of num or expected_coverage given
-        next(rand_reads(S, len_mean=50, num=1, expected_coverage=1))
+        next(rand_read(S, len_mean=50, num=1, expected_coverage=1))
+
+    assert sum(1 for _ in rand_read(S, len_mean=50)) == 1, \
+        'If neither num or expected coverage is given only one sample is read'
 
     # there should be no noise added
-    read, pos = next(rand_reads(S, len_mean=40, num=1))
+    read, pos = next(rand_read(S, len_mean=40, num=1))
     assert S[pos:pos+len(read)] == read
+
+    S = A.parse('ACT' * 100)
+    reads = [x for x in rand_read(S, len_mean=100, len_sd=0.01, num=100)]
+    assert set(len(read) for read, _ in reads) > 1, \
+        'Read lengths should be randomly chosen'
+    len_mean = sum(len(read) for read, _ in reads) / 100.
+    assert len_mean > 50 and len_mean < 150, \
+        'Normal distribution of read lengths works'
 
     # index edge cases
     A = Alphabet(['00', '01'])
     S = A.parse('01' * 10)
     _bak = np.random.normal
     np.random.normal = mock.Mock(return_value=[1])
-    assert next(rand_reads(S, len_mean=1, num=1))[0] == A.parse('01')
+    assert next(rand_read(S, len_mean=1, num=1))[0] == A.parse('01'), \
+        'sequences in alphabets with > 1 long letters can be sampled too'
     np.random.normal = _bak
+
+
+def test_lossy_reads():
+    A = Alphabet('ACGT')
+    S = A.parse('ACT' * 100)
+    gap_kw = {'go_prob': 0.2, 'ge_prob': 0.3}
+    M = MutationProcess(A, subst_probs=0.3, **gap_kw)
+    read, pos, tx = next(M.noisy_read(S, len_mean=10, num=1))
+    assert tx.count('S') > 0 and tx.count('I') + tx.count('D') > 0, \
+        'Random mutations should be performed to get lossy reads'
 
 
 def test_mutation_process():
