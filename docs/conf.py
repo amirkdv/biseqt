73222c46d5f1256129cfc885f46ec556b430242d
Amir Kadivar <amir@amirkdv.ca>
2016-07-03 15:09:16 -0400

    alabaster theme adjustments

--- docs/conf.py
+++ docs/conf.py
@@ -202,6 +202,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
+html_sidebars = {'*': ['searchbox.html', 'localtoc.html', 'navigation.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
