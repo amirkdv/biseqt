369768491a5024c3ddc52d6c67985d3fe6f9703a
Amir Kadivar <amir@amirkdv.ca>
2018-02-07 12:04:16 -0500

    change usage of SeedIndex, index upon init
    uses the existence of its index table as an indicator

--- biseqt/seeds.py
+++ biseqt/seeds.py
@@ -37,20 +37,40 @@ class SeedIndex(KmerDBWrapper):
     Attributes:
         kmer_index (KmerIndex): The kmer index to operate on.
     """
-    def __init__(self, path=':memory:', alphabet=None,
+    def __init__(self, S, T, path=':memory:', alphabet=None,
                  wordlen=None, log_level=logging.INFO):
         super(SeedIndex, self).__init__(path=path, wordlen=wordlen,
                                         alphabet=alphabet, log_level=log_level)
+        self.self_comp = S == T
+        self.S, self.T = S, T
+        self.table = 'seeds_%s_%s' % (S.content_id[:8], T.content_id[:8])
+        self.d0 = len(self.T) - 1
+        if self._table_exists():
+            self.log('seeds for %s and %s already indexed, skipping' %
+                     (S.content_id[:8], T.content_id[:8]))
+        else:
+            self.log('Indexing seeds for %s and %s.' %
+                     (S.content_id[:8], T.content_id[:8]))
+            self._index_seeds()
 
-    def table_name(self, S, T):
-        return 'seeds_%s_%s' % (S.content_id[:8], T.content_id[:8])
+    @classmethod
+    def to_diagonal_coordinates(cls, i, j):
+        d = i - j
+        a = min(i, j)
+        return d, a
 
-    def _table_exists(self, table):
+    @classmethod
+    def to_ij_coordinates(cls, d, a):
+        i = a + max(d, 0)
+        j = a - min(d, 0)
+        return (i, j)
+
+    def _table_exists(self):
         with self.connection() as conn:
             q = """
                 SELECT name FROM sqlite_master
                 WHERE type='table' AND name='%s';
-            """ % table
+            """ % self.table
             cursor = conn.cursor()
             cursor.execute(q)
             for name in cursor:
@@ -58,7 +78,7 @@ class SeedIndex(KmerDBWrapper):
         return False
 
     # idempotent operation
-    def index_seeds(self, S, T):
+    def _index_seeds(self):
         """Indexes all seeds and their diagonal positions. For each kmer with
         :math:`n` hits from *distinct* sequences :math:`n\\choose2` seeds
         are created. If a kmer occurs multiple times in a sequence seeds
@@ -74,60 +94,45 @@ class SeedIndex(KmerDBWrapper):
                 <biseqt.kmers.KmerIndex.score_kmers>`). Default is None in
                 which case all kmers are considered.
         """
-        table = self.table_name(S, T)
-        if self._table_exists(table):
-            self.log('seeds for %s and %s already indexed, skipping' %
-                     (S.content_id[:8], T.content_id[:8]))
-            return
-
         with self.connection() as conn:
             conn.cursor().execute("""
                 CREATE TABLE %s (
                   'd_' INTEGER,     -- zero-adjusted diagonal position
                   'a'  INTEGER      -- antidiagonal position
                 );
-            """ % table)
+            """ % self.table)
 
         # FIXME the kmers table shouldn't be reused, use a separate file but
         # stay in memory if we are :memory:
         kmer_index = KmerIndex(self.path, wordlen=self.wordlen,
                                alphabet=self.alphabet)
-        self_comp = S == T
-        kmer_index.index_kmers(S)
-        if not self_comp:
-            kmer_index.index_kmers(T)
+        kmer_index.index_kmers(self.S)
+        if not self.self_comp:
+            kmer_index.index_kmers(self.T)
 
         def _records():
             kmers = kmer_index.kmers()
             for kmer in kmers:
                 hits = kmer_index.hits(kmer)
-                # FIXME check integrity of id0 and id1? we should behave
-                # differently if we are expected to work under S vs S
-                # comparison
-                d0 = len(T) - 1
                 for (id0, pos0), (id1, pos1) in combinations(hits, 2):
-                    if not self_comp and id0 == id1:
+                    if not self.self_comp and id0 == id1:
                         continue
-                    d = pos0 - pos1
-                    d_ = d + d0
-                    a = min(pos0, pos1)
-                    yield d_, a
+                    d, a = self.to_diagonal_coordinates(pos0, pos1)
+                    yield d + self.d0, a
 
-        self.log('Indexing seeds.')
         with self.connection() as conn:
             cursor = conn.cursor()
             cursor.executemany(
-                'INSERT INTO %s (d_, a) VALUES (?, ?)' % table,
+                'INSERT INTO %s (d_, a) VALUES (?, ?)' % self.table,
                 _records()
             )
             self.log('Creating SQL index for seeds table.')
             cursor.execute('CREATE INDEX %s_diagonal ON %s(d_);' %
-                           (table, table))
+                           (self.table, self.table))
 
-    def seed_count_by_d_(self, S, T):
-        self.index_seeds(S, T)
-        q = 'SELECT COUNT(a), d_ FROM %s GROUP BY d_' % self.table_name(S, T)
-        count_by_d_ = np.zeros(len(S) + len(T))
+    def seed_count_by_d_(self):
+        q = 'SELECT COUNT(a), d_ FROM %s GROUP BY d_' % self.table
+        count_by_d_ = np.zeros(len(self.S) + len(self.T))
         with self.connection() as conn:
             cursor = conn.cursor()
             cursor.execute(q)
@@ -135,24 +140,22 @@ class SeedIndex(KmerDBWrapper):
                 count_by_d_[d_] = count
         return count_by_d_
 
-    def seed_in_band_count_by_a(self, S, T, center, radius):
-        self.index_seeds(self, S, T)
+    def seed_in_band_count_by_a(self, center, radius):
         q = """
             SELECT COUNT(d_), a FROM %s
             WHERE d_ - ? BETWEEN ? AND ?
             GROUP BY a
-        """ % self.table_name(S, T)
-        d0 = len(T) - 1
-        count_by_a = np.zeros(min(len(S), len(T)))
+        """ % self.table
+        count_by_a = np.zeros(min(len(self.S), len(self.T)))
         with self.connection() as conn:
             cursor = conn.cursor()
-            cursor.execute(q, (d0, center - radius, center + radius))
+            cursor.execute(q, (self.d0, center - radius, center + radius))
             for count, a in cursor:
                 count_by_a[a] = count
 
         return count_by_a
 
-    def seeds(self, S, T, d_center=None, d_radius=None):
+    def seeds(self, d_center=None, d_radius=None):
         """Yields the :class:`seeds <Seed>` and their respective scores for a
         given pair of sequences and a given diagonal range (cf.
         :func:`highest_scoring_band`). Only seeds that are processed by
@@ -172,20 +175,16 @@ class SeedIndex(KmerDBWrapper):
                 The :class:`Seed` object and the score of its diagonal as a
                 ``float`` in descending order of score.
         """
-        self.index_seeds(S, T)
-        d0 = len(T) - 1
-        query = 'SELECT d_, a FROM %s' % self.table_name(S, T)
+        query = 'SELECT d_, a FROM %s' % self.table
         if d_center and d_radius:
             query += ' WHERE d_ - %d BETWEEN %d AND %d ' % \
-                (d0, d_center - d_radius, d_center + d_radius)
+                (self.d0, d_center - d_radius, d_center + d_radius)
 
-        self_comp = S == T
         with self.connection() as conn:
             cursor = conn.cursor()
             cursor.execute(query)
             for d_, a in cursor:
-                d = d_ - d0
-                i, j = (a + max(d, 0), a - min(d, 0))
+                i, j = self.to_ij_coordinates(d_ - self.d0, a)
                 yield (i, j)
-                if self_comp and i != j:
+                if self.self_comp and i != j:
                     yield (j, i)
--- tests/test_seeds.py
+++ tests/test_seeds.py
@@ -1,50 +1,53 @@
 # -*- coding: utf-8 -*-
-import pytest
-
 from biseqt.sequence import Alphabet
 from biseqt.seeds import SeedIndex
 
 
-@pytest.fixture
-def seed_index():
-    """Creates a database, a kmer index, and a seed index with word length 5
-    stored in memory and returns the seed index. The database is populated with
-    3 random sequences of length 100 and all kmers and seeds are indexed."""
+def test_coordinate_change():
+    pairs = {  # (i,j) => (d, a)
+        (0, 0):     (0, 0),
+        (10, 0):    (10, 0),
+        (0, 10):    (-10, 0),
+        (10, 10):   (0, 10)
+    }
+    for (i, j), (d, a) in pairs.items():
+        assert SeedIndex.to_diagonal_coordinates(i, j) == (d, a), \
+            'conversion to diagonal coordinates should be correct'
+        assert SeedIndex.to_ij_coordinates(d, a) == (i, j), \
+            'conversion from diagonal coordinates should be correct'
+
+
+def test_index_seeds():
     A = Alphabet('ACGT')
     wordlen = 5
+    kw = {'alphabet': A, 'wordlen': wordlen, 'path': ':memory:'}
 
-    seed_index = SeedIndex(path=':memory:', wordlen=wordlen, alphabet=A)
-    return seed_index
-
-
-def test_index_seeds(seed_index):
-    A = seed_index.alphabet
-    wordlen = seed_index.wordlen
     S = A.parse('G' * wordlen)
     T = A.parse('TC' + str(S))
-    assert list(seed_index.seeds(S, T)) == [(0, 2)] and \
-        list(seed_index.seeds(T, S)) == [(2, 0)], \
+    assert list(SeedIndex(S, T, **kw).seeds()) == [(0, 2)] and \
+        list(SeedIndex(T, S, **kw).seeds()) == [(2, 0)], \
         'seed coordinates should be correctly calculated'
 
-    S = seed_index.alphabet.parse('A' * 10)
-    T = seed_index.alphabet.parse('A' * 20)
+    S = A.parse('A' * 10)
+    T = A.parse('A' * 20)
     n_seeds = (len(S) - wordlen + 1) * (len(T) - wordlen + 1)
-    assert len(list(seed_index.seeds(S, T))) == n_seeds, \
+    assert len(list(SeedIndex(S, T, **kw).seeds())) == n_seeds, \
         'correct number of seeds should be found'
     n_kmers = (len(S) - wordlen + 1)
     n_seeds = n_kmers * n_kmers - n_kmers  # don't count (i, i) twice
-    assert len(list(seed_index.seeds(S, S))) == n_seeds, \
+    assert len(list(SeedIndex(S, S, **kw).seeds())) == n_seeds, \
         'self comparison should not get confused'
 
 
-def test_count_seeds_on_diagonals(seed_index):
-    A = seed_index.alphabet
-    wordlen = seed_index.wordlen
+def test_count_seeds_on_diagonals():
+    A = Alphabet('ACGT')
+    wordlen = 5
+    kw = {'alphabet': A, 'wordlen': wordlen, 'path': ':memory:'}
     S = A.parse('G' * wordlen)
     T = A.parse('TT' + str(S))
     d0 = len(T) - 1
 
-    count_by_d_ = seed_index.seed_count_by_d_(S, T)
+    count_by_d_ = SeedIndex(S, T, **kw).seed_count_by_d_()
     expected_count = [1 if i - d0 == -2 else 0
                       for i in range(len(S) + len(T))]
     assert list(count_by_d_) == expected_count, \
