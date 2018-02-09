cd56a1dcce01ca8d4bec8af137c7d07448aa16c3
Amir Kadivar <amir@amirkdv.ca>
2018-02-08 23:18:56 -0500

    add "conserved sequence" experiment

--- experiments/blot_stats.py
+++ experiments/blot_stats.py
@@ -60,22 +60,12 @@ def pad_seq_pair(S, T, pad_length):
     S_, T_ = rand_seq(ALPHABET, pad_length), rand_seq(ALPHABET, pad_length)
     return S + S_, T + T_
 
-def plot_seeds(ax, seeds, N0, N1, c='k'):
-    idx_S, idx_T = [], []
-    for seed in seeds:
-        idx_S.append(seed[0])
-        idx_T.append(seed[1])
-
-    # x and y are flipped when going from matrix notation to plotting.
-    ax.scatter(idx_T, idx_S, facecolor=c, edgecolor=None, lw=0, s=2, alpha=.3)
+def adjust_pw_plot(ax, N0, N1):
     ax.set_ylim(-N0 * .1, N0 * 1.1)
     ax.set_xlim(-N1 * .1, N1 * 1.1)
-
-    ax.grid(True)
     ax.set_xlabel('Pos. in T', fontsize=12)
     ax.set_ylabel('Pos. in S', fontsize=12)
     ax.set_aspect('equal')
-
     line_kw = {'c': 'k', 'alpha': .1, 'lw':'5'}
     ax.axvline(x=-5,  **line_kw)
     ax.axvline(x=N1+5, **line_kw)
@@ -86,7 +76,19 @@ def plot_seeds(ax, seeds, N0, N1, c='k'):
     ax.xaxis.set_label_position('top')
     ax.tick_params(labelsize=10)
 
-def plot_similar_segment(ax, segment):
+
+# FIXME absorb as much as possible into SeedIndex
+def plot_seeds(ax, seeds, N0, N1, c='k'):
+    idx_S, idx_T = [], []
+    for seed in seeds:
+        idx_S.append(seed[0])
+        idx_T.append(seed[1])
+
+    # x and y are flipped when going from matrix notation to plotting.
+    ax.scatter(idx_T, idx_S, facecolor=c, edgecolor=None, lw=0, s=2, alpha=.3)
+    ax.grid(True)
+
+def plot_similar_segment(fig, ax, segment, color):
     (d_min, d_max), (a_min, a_max) = segment
     seg_ds = [d_min, d_min, d_max, d_max]
     seg_as = [a_min, a_max, a_max, a_min]
@@ -99,7 +101,7 @@ def plot_similar_segment(ax, segment):
         # x and y is flipped between matrix notation and plotting
         seg_xs[i], seg_ys[i] = y, x
 
-    ax.fill(seg_xs, seg_ys, c='r', lw=5, alpha=.3)
+    im = ax.fill(seg_xs, seg_ys, c=color, lw=5, alpha=.3)
 
 
 # =======================
@@ -113,6 +115,7 @@ def plot_stats_performance(fig, K, ns, n_samples, real=False, wordlen=None,
     if replot:
         results = pickle_load(dumpfile)
     else:
+        M = MutationProcess(ALPHABET, subst_probs=subst, ge_prob=gap, go_prob=gap)
         shape = (len(ns), n_samples)
         HF_kw = {'gap_prob': gap, 'subst_prob': subst,
                  'sensitivity': .9, 'alphabet': ALPHABET, 'wordlen': wordlen,
@@ -126,7 +129,7 @@ def plot_stats_performance(fig, K, ns, n_samples, real=False, wordlen=None,
         for n_idx, n in enumerate(ns):
             print 'n = %d (%d/%d)' % (n, n_idx+1, len(ns))
             for i in range(n_samples):
-                S, T = rand_seq_pair(K, real=real, related=False, gap=gap, subst=subst)
+                S, T = rand_seq_pair(K, real=real, related=False, mutation_process=M)
                 S, T = pad_seq_pair(S, T, n-K)
                 HF = HomologyFinder(S, T, **HF_kw)
                 if banded:
@@ -141,7 +144,7 @@ def plot_stats_performance(fig, K, ns, n_samples, real=False, wordlen=None,
                 results['H1']['neg'][n_idx][i] = s1
 
             for i in range(n_samples):
-                S, T = rand_seq_pair(K, real=real, related=True, gap=gap, subst=subst)
+                S, T = rand_seq_pair(K, real=real, related=True, mutation_process=M)
                 S, T = pad_seq_pair(S, T, n-K)
                 HF = HomologyFinder(S, T, **HF_kw)
                 if banded:
@@ -213,40 +216,109 @@ def exp_stats_performance():
     fig.savefig('stats performance - banded K=%d.png' % K, dpi=300)
 
 def exp_local_alignment():
-    gap = .1
-    subst = .1
+    gap = .15
+    subst = .15
     K = 500
     wordlen = 8
+    # NOTE I can drive sensitivity to 0 and get decent results
     HF_kw = {'gap_prob': gap, 'subst_prob': subst,
              'sensitivity': .9, 'alphabet': ALPHABET, 'wordlen': wordlen,
-             'path': ':memory:', 'log_level': logging.WARNING}
+             'path': 'example.db', 'log_level': logging.INFO}
 
     M = MutationProcess(ALPHABET, subst_probs=subst, ge_prob=gap, go_prob=gap)
+    M_hgt = MutationProcess(ALPHABET, subst_probs=subst/3, ge_prob=gap/3, go_prob=gap/3)
 
     homs = [rand_seq(ALPHABET, i) for i in [200, 500, 1500, 2500]]
     junk = lambda: rand_seq(ALPHABET, np.random.randint(1000, 2000))
-    S = homs[0] + junk() + homs[1] + junk() + homs[3] + junk() + \
-        homs[0] + junk() + homs[2] + junk() + homs[0] + junk()
-    Ms = [M.mutate(hom) for hom in homs]
+
+    S = junk() + homs[0] + junk() + homs[1] + junk() + homs[3] + \
+        junk() + homs[2] + junk() + homs[2] + junk() + homs[0] + junk()
+    homs = [M.mutate(homs[i])[0] if i != 2 else M_hgt.mutate(homs[i])[0]
+            for i in range(len(homs))]
     T = junk() + homs[3] + junk() + homs[2] + junk() + homs[0] + \
         junk() + homs[3] + junk() + homs[1] + junk() + homs[2] + junk()
 
 
-    #K = 100
-    #S, T = rand_seq_pair(2*K, related=True, gap=gap, subst=subst)
-
-    HF = HomologyFinder(S, T, **HF_kw)
-
+    import matplotlib.gridspec as gridspec
     fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
+    gs = gridspec.GridSpec(1, 2, width_ratios=[30,1])
+    ax = plt.subplot(gs[0])
+    ax_colorbar = plt.subplot(gs[1])
+
+    import matplotlib
+    cmap = plt.cm.get_cmap('jet')
+
+    max_score = 1
+
+    print 'finding homologies'
+    HF = HomologyFinder(S, T, **HF_kw)
+    scores = []
+    for segment, score in HF.similar_segments(K):
+        (d_min, d_max), (a_min, a_max) = segment
+        print segment, score
+        color = cmap(score/max_score)[:3]
+        plot_similar_segment(fig, ax, segment, color=color)
+        scores.append(score)
+
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=max_score)
     plot_seeds(ax, HF.seeds(), len(S), len(T))
-    for segment in HF.similar_segments(K):
-        print segment
-        plot_similar_segment(ax, segment)
+    adjust_pw_plot(ax, len(S), len(T))
+
+    cb1 = matplotlib.colorbar.ColorbarBase(ax_colorbar, cmap=cmap, norm=norm, orientation='vertical')
+
     fig.tight_layout()
-    fig.savefig('local-alignment.png', dpi=500) # NOTE LOCAL ALIGNMENT WORKS !!! (Wed, 17 Jan 2018)
+    fig.savefig('local-alignment.png', dpi=300) # NOTE LOCAL ALIGNMENT WORKS !!! (Wed, 17 Jan 2018)
+
+def exp_conserved_sequences():
+    gap = .15
+    subst = .15
+    K = 500
+    wordlen = 8
+    HF_kw = {'gap_prob': gap, 'subst_prob': subst,
+             'sensitivity': .9, 'alphabet': ALPHABET, 'wordlen': wordlen,
+             'path': 'example.db', 'log_level': logging.INFO}
+
+    M_std = MutationProcess(ALPHABET, subst_probs=.2, ge_prob=.15, go_prob=.15)
+    M_con = MutationProcess(ALPHABET, subst_probs=.05, ge_prob=.05, go_prob=.05)
+
+    pieces = [rand_seq(ALPHABET, 10000) for _ in range(3)]
+    conserved = rand_seq(ALPHABET, 1000)
+
+    S = pieces[0] + conserved + pieces[1] + pieces[2]
+    pieces_hom = [M_std.mutate(p)[0] for p in pieces]
+    conserved_hom = M_con.mutate(conserved)[0]
+    T = pieces_hom[0] + pieces_hom[1] + conserved_hom + pieces_hom[2]
+
+    import matplotlib.gridspec as gridspec
+    fig = plt.figure()
+    gs = gridspec.GridSpec(1, 2, width_ratios=[30,1])
+    ax = plt.subplot(gs[0])
+    ax_colorbar = plt.subplot(gs[1])
+
+    import matplotlib
+    cmap = plt.cm.get_cmap('jet')
+
+    print 'finding homologies'
+    HF = HomologyFinder(S, T, **HF_kw)
+    scores = []
+    for segment, score, match_prob in HF.similar_segments(K):
+        (d_min, d_max), (a_min, a_max) = segment
+        print segment, score, match_prob
+        color = cmap(match_prob)[:3]
+        plot_similar_segment(fig, ax, segment, color=color)
+        scores.append(score)
+
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=1)
+    plot_seeds(ax, HF.seeds(), len(S), len(T))
+    adjust_pw_plot(ax, len(S), len(T))
+
+    cb1 = matplotlib.colorbar.ColorbarBase(ax_colorbar, cmap=cmap, norm=norm, orientation='vertical')
+
+    fig.tight_layout()
+    fig.savefig('conserved-sequences.png', dpi=300) # NOTE LOCAL ALIGNMENT WORKS !!! (Wed, 17 Jan 2018)
 
 
 if __name__ == '__main__':
     #exp_stats_performance()
-    exp_local_alignment()
+    #exp_local_alignment()
+    exp_conserved_sequences()
