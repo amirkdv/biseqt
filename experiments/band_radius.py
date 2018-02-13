fix standard deviation region for bounded data

experiments/band_radius.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from bisect import bisect_left
 from scipy.special import erf, erfcinv
-from util import color_code, with_dumpfile, savefig
+from util import color_code, plot_with_sd, with_dumpfile, savefig
 
 
 def time_in_band(K, g, r):
@@ -78,14 +78,7 @@ def plot_time_in_band(sim_data, cutoff_epsilon, path=None):
 
         res = sim_data['in_band'][g_idx, :, :]
 
-        num_sds = 1
-        means = res.mean(axis=1)
-        sds = np.sqrt(res.var(axis=1))
-        ys_l = means - num_sds * sds
-        ys_h = np.minimum(1, means + num_sds * sds)
-        ax_sim.plot(rs, means, lw=1.5, color=color, zorder=2)
-        ax_sim.fill_between(rs, ys_l, ys_h, facecolor=color, edgecolor=color,
-                            alpha=.2)
+        plot_with_sd(ax_sim, rs, res, axis=1, y_max=1, color=color, lw=1.5)
         ax_sim.set_xlabel('Band radius')
         ax_sim.set_ylabel('proportion of time in band')
         ax_sim.axvline(r_cutoff, color=color, lw=5, alpha=.3)

experiments/util.py
@@ -67,7 +67,7 @@ def with_dumpfile(func):
     def wrapped_func(*args, **kwargs):
         dumpfile = kwargs['dumpfile']
         path = os.path.join(DUMP_DIR, dumpfile)
-        if os.path.exists(path) and not kwargs.get('repeat', False):
+        if os.path.exists(path) and not kwargs.get('ignore_existing', False):
             return pickle_load(dumpfile)
         out = func(*args, **kwargs)
         pickle_dump(dumpfile, out)
@@ -86,16 +86,21 @@ def color_code(values, cmap='rainbow'):
     return [cmap((v - m) / M) for v in values]
 
 
-def plot_with_sd(ax, xs, ys, num_sds=1, axis=None, color='k', **plot_kw):
+def plot_with_sd(ax, xs, ys, axis=None, n_sds=1, y_max=None, color='k', **kw):
     """Plots the mean of given data set (with specified axis) and add a shaded
     region around specifying a given number of standard deviations."""
     means = ys.mean(axis=axis)
     sds = np.sqrt(ys.var(axis=axis))
 
-    ys_l = means - num_sds * sds
-    ys_h = means + num_sds * sds
+    ys_l = means - n_sds * sds
+    ys_h = means + n_sds * sds
 
-    ax.plot(xs, means, color=color, **plot_kw)
+    if y_max is not None:
+        assert np.all(means <= y_max), 'all means must be bounded above by max'
+        ys_l -= np.maximum(0, ys_h - y_max)
+        ys_h = np.minimum(ys_h, y_max)
+
+    ax.plot(xs, means, color=color, **kw)
     ax.fill_between(xs, ys_l, ys_h, facecolor=color, edgecolor=color, alpha=.2)
 
 