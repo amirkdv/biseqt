097ff0292a912fc3838ce6bda332d3de2018b706
Amir Kadivar <amir@amirkdv.ca>
2018-02-26 09:10:22 -0500

    add word blot logo, update .gitignore

---
+++ experiments/blot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env
+from math import sqrt
+from matplotlib import pyplot as plt
+import random
+from itertools import product
+
+
+def normalize(x, y):
+    norm = sqrt(x**2 + y**2)
+    return x / norm, y / norm
+
+
+resolution = 20
+moves_1d = [i for i in range(-resolution, resolution) if i]
+moves_2d = [normalize(x, y) for x, y in product(moves_1d, moves_1d)]
+
+
+def rw(n_steps):
+    x, y = 0, 0
+    xs, ys = [], []
+    for _ in range(n_steps):
+        dx, dy = random.choice(moves_2d)
+        x, y = x + dx, y + dy
+        xs.append(x)
+        ys.append(y)
+
+    return xs, ys
+
+
+n_steps = 1000
+n_paths = 1000
+
+fig = plt.figure(figsize=(4, 4))
+ax = fig.add_subplot(1, 1, 1)
+for i in range(n_paths):
+    if (i + 1) % 100 == 0:
+        print '%d/%d' % (i+1, n_paths)
+    xs, ys = rw(n_steps)
+    ax.plot(xs, ys, c='k', alpha=.1, lw=.5)
+
+ax.set_aspect('equal')
+ax.axis('off')
+fig.tight_layout()
+fig.savefig('blot.png', transparent=True, bbox_inches='tight', pad_inches=0.0,
+            dpi=300)
