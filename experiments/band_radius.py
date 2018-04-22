5454b0b007361a12aca1cdd01c2284ea5f672100
Amir Kadivar <amir@amirkdv.ca>
2018-04-22 00:03:19 -0400

    better radius experiments

--- experiments/band_radius.py
+++ experiments/band_radius.py
@@ -3,21 +3,23 @@ import numpy as np
 from matplotlib import gridspec
 from matplotlib import pyplot as plt
 from bisect import bisect_left
-from scipy.special import erf
+from scipy.special import erf, erfcinv
 from scipy.ndimage.filters import gaussian_filter1d
 import re
 
 from util import log, color_code, plot_with_sd, with_dumpfile, savefig
 from util import sample_opseq, estimate_gap_probs_in_opseq
-from util import load_fasta
+from util import plot_global_alignment, adjust_pw_plot
+from util import load_fasta, opseq_path
 
+from biseqt.seeds import SeedIndex
 from biseqt.blot import band_radius
 
 
-def time_in_band(K, g, r):
-    A = r / (np.sqrt(2 * g * K))
-    return erf(A) \
-        + A * (2 / np.sqrt(np.pi) * np.exp(-A ** 2) - 4 * A * (1 - erf(A)))
+def time_in_band(K, gap, radius):
+    area = radius / (np.sqrt(2 * gap * K))
+    return erf(area) + area * (2 / np.sqrt(np.pi) * np.exp(-area ** 2) -
+                               4 * area * (1 - erf(area)))
 
 
 def sample_edit_sequences(K, g, n_samples, bio=False):
@@ -70,23 +72,26 @@ def sim_time_in_band(K, gs, rs, n_samples, **kw):
     return sim_data
 
 
-def plot_time_in_band(sim_data, cutoff_epsilon, path=None):
-    assert path
+def plot_time_in_band(sim_data, cutoff_epsilon):
     gs = sim_data['gs']
     rs = sim_data['rs']
     K = sim_data['K']
+    n_samples = sim_data['in_band']['simulated'].shape[2]
 
-    fig = plt.figure(figsize=(10, 6))
+    fig = plt.figure(figsize=(10, 8))
 
-    grids = gridspec.GridSpec(2, 2, width_ratios=[1.5, 1])
+    grids = gridspec.GridSpec(3, 2 , height_ratios=[1, 1, 1])
 
-    ax_mod = fig.add_subplot(grids[:, 0])  # model
-    ax_sim = fig.add_subplot(grids[0, 1])  # simulation
-    ax_bio = fig.add_subplot(grids[1, 1])  # biological data
+    ax_mod = fig.add_subplot(grids[:2, 0])  # model
+    ax_sim = fig.add_subplot(grids[2, 0])  # simulated data
+    ax_bio = fig.add_subplot(grids[2, 1])  # biological data
+    ax_rw1 = fig.add_subplot(grids[0, 1])
+    ax_rw2 = fig.add_subplot(grids[1, 1])
 
     colors = color_code(gs, cmap='PuOr')
 
-    for g_idx, (color, g) in enumerate(zip(colors, gs)):
+    for g_idx, (color, g, ax_rw) in enumerate(zip(colors, gs,
+                                                  [ax_rw1, ax_rw2])):
         r_lim = rs[bisect_left(
             sim_data['in_band']['biological'][g_idx].mean(axis=1),
             1 - .5 * cutoff_epsilon
@@ -101,12 +106,37 @@ def plot_time_in_band(sim_data, cutoff_epsilon, path=None):
         ax_mod.plot(rs, us, ls='--', **kw)                 # full correct model
         ax_mod.axvline(r_cutoff, color=color, lw=5, alpha=.3)
 
+        ax_mod.set_title('model predictions', fontsize=10)
         ax_mod.grid(True)
         ax_mod.set_xlabel('diagonal band radius')
         ax_mod.set_ylabel('proportion of time in band')
         ax_mod.legend(loc='lower right', fontsize=12)
         ax_mod.set_xlim(0, r_lim)
         ax_mod.set_ylim(0, 1.2)
+
+        kw = {'lw': 1, 'alpha': .2, 'color': color}
+        for opseq in sample_edit_sequences(K, g, n_samples, bio=False):
+            xs, ys = opseq_path(opseq, x0=0, y0=0)
+            ds = [SeedIndex.to_diagonal_coordinates(x, y)[0]
+                  for x, y in zip(xs, ys)]
+            ax_rw.plot(range(K + 1), ds, **kw)
+
+        as_ = np.arange(0, 2 * K)
+        for eps, lw, alpha, label in zip([1e-1, 1e-6], [2, 3], [.7, .3],
+                                     ['$\epsilon=10^{-1}$',
+                                      '$\epsilon=10^{-6}$']):
+
+            def radius_(k): return erfcinv(eps) * np.sqrt(2 * g) * np.sqrt(k)
+
+            plot_kw = {'c': 'k', 'alpha': alpha, 'lw': lw}
+            radii = np.array([radius_(k) for k in range(K)])
+            ax_rw.plot(range(K), radii, label=label, **plot_kw)
+            ax_rw.plot(range(K), -radii, **plot_kw)
+
+        ax_rw.legend(loc='upper left', fontsize=8)
+        ax_rw.set_ylim(-50, 50)
+        ax_rw.set_xlabel('number of alignment steps')
+        ax_rw.set_ylabel('diagonal position $d$')
 
         for key, ax in zip(['simulated', 'biological'], [ax_sim, ax_bio]):
             res = sim_data['in_band'][key][g_idx, :, :]
@@ -126,20 +156,12 @@ def plot_time_in_band(sim_data, cutoff_epsilon, path=None):
             ax.set_ylabel('proportion of time in band', fontsize=8)
             ax.legend(loc='lower right', fontsize=8)
 
-    # n_samples = sim_data['in_band']['simulated'].shape[2]
-    # fig.suptitle('$K = %d$, no. samples = $%d$' % (K, n_samples))
-    # fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-    savefig(fig, path, comment='time in band simulation')
+    savefig(fig, 'band_radius.png', comment='time in band')
 
 
 def exp_time_in_band():
-    """Shows proportion of alignment time (i.e. number of edit operations)
-    spent within diagonal band for:
-
-    * Simulated edit operations,
-    * Biological edit operations obtained from real aligned genomes,
-    * Full probabilistic model (expected amount of time spent in band),
-    * Approximated probabilistic model (probability of last step in band).
+    """Shows theoretical and empirical results for proportion of alignment time
+    (i.e. number of edit operations) spent within diagonal band.
 
     **Supported Claims**
 
@@ -158,20 +180,25 @@ def exp_time_in_band():
         https://www.dropbox.com/s/93cg9t4oh0h2guh/band_radius.png?raw=1
        :alt: lightbox
 
-       Predicted proportion of time spent in diagonal band (left) in
-       alignments of total length K=500 according to full (dashed)
-       and approximate (solid) probabilistic model and for two values of
-       gap probabilities. Horizontal lines indicate the calculated band
-       radius for %.01 accuracy. Empericial observations (n=500 samples)
-       for proportion of time spent in band based on simulated edit sequences
-       (top right) and real biological data (bottom right) are shown, and in
-       each case the empirical percentage of time spent *outside* of band is
-       reported; shaded regions indicate one standard deviation.  Biological
-       alignments are chosen from projected pairwise alignments of *Actinin 2*
-       for 7 vertebrates obtained from UCSC genome browser. Each sample
-       biological edit sequence is chosen such that it has the desired
-       length and gap probability; gaps of length more than 10 nucleotides
-       are removed to avoid nonstationary gap probabilities.
+       Predicted proportion of time spent in diagonal band (*top left*) in
+       alignments of total length K=500 according to full (dashed) and
+       approximate (solid) probabilistic model and for two values of gap
+       probabilities. Horizontal lines indicate the calculated band radius for
+       %.01 accuracy.
+       Simulated alignments (n=500 samples) are drawn in diagonal coordinates
+       as a function of alignment time for gap probability 0.05 (*right top*)
+       and 0.15 (*right middle*) with bounds obtained from
+       :func:`biseqt.blot.band_radius` for two sensitivity values.
+       Proportion of time spent in band as a function of band radius is shown
+       for simulated sequences (*bottom left*) and real biological data
+       (*bottom right*), and in each case the empirical percentage of time
+       spent *outside* of band is reported; shaded regions indicate one
+       standard deviation.  Biological alignments are chosen from projected
+       pairwise alignments of *Actinin 2* for 7 vertebrates obtained from
+       UCSC genome browser. Each sample biological edit sequence is chosen
+       such that it has the desired length and gap probability; gaps of
+       length more than 10 nucleotides are removed to avoid nonstationary
+       gap probabilities.
     """
     K = 500
     gs = [.05, .15]
@@ -180,49 +207,89 @@ def exp_time_in_band():
     cutoff_epsilon = 1e-4  # for vertical lines showing calculated cutoff
 
     dumpfile = 'band_radius.txt'
-    plot_path = 'band_radius.png'
-    sim_data = sim_time_in_band(K, gs, rs, n_samples, dumpfile=dumpfile)
-    plot_time_in_band(sim_data, cutoff_epsilon, path=plot_path)
+    sim_data = sim_time_in_band(K, gs, rs, n_samples, dumpfile=dumpfile, ignore_existing=True)
+    plot_time_in_band(sim_data, cutoff_epsilon)
 
 
 def exp_gap_probs():
-    opseqs_path = 'data/acta2/acta2-7vet-opseqs.fa'
+    """Shows gap probability variability in simulated and biological edit
+    sequences.
 
-    with open(opseqs_path) as f:
-        opseq = f.read().strip()
+    **Supported Claims**
 
-    fig = plt.figure(figsize=(5, 5))
-    ax = fig.add_subplot(1, 1, 1)
+    * Gap probabilities are nonstationary in biological data.
+    * Removing long indel stretches mitigates this nonstationarity for the most
+      part and allows us to treat gap probability in biological alignments
+      as stationary.
 
-    K = 2000
-    sample_len = 2 * K
-    gap_hat = 0
-    while gap_hat < .05:
-        start = np.random.randint(0, len(opseq) - sample_len)
-        sample = opseq[start:start + sample_len]
-        gap_hat = 1. * (sample.count('I') + sample.count('D')) / sample_len
+    .. figure::
+        https://www.dropbox.com/s/vplg0x22ykc47am/gap_probabilities.png?raw=1
+       :target:
+        https://www.dropbox.com/s/vplg0x22ykc47am/gap_probabilities.png?raw=1
+       :alt: lightbox
 
-    g = gap_hat
-    sim_sample = ''.join(np.random.choice(list('MID'),
-                                          p=[1 - g, g / 2, g / 2],
-                                          size=sample_len))
+       Gap probability variability along alignments (length K=1200) in
+       simulated (black) and biological edit sequences, before (left) and after
+       (right) removing long indel stretches (deletion or insertion stretches
+       of length at least 10) from biological samples; n=50 samples in both
+       cases. Local gap probabilites are calculated in a window of radius 100
+       nt.  Simulated edit sequences (black) are generated using stationary gap
+       probability (i.e constant along alignment) of 0.15.  Biological edit
+       sequences are sampled from projected pairwise alignments of *Actinin 2*
+       for 7 vertebrates obtained from UCSC genome browser such that the
+       overall gap probability (i.e over K=1200 operations) is 0.15.
+    """
+    pws_path = 'data/actn2/actn2-7vet-pws.fa'
+    full_opseq = ''
+    with open(pws_path) as f:
+        for seq, name, _ in load_fasta(f):
+            full_opseq += seq
 
-    radius = 200
-    gaps = estimate_gap_probs_in_opseq(sample, radius)
-    ax.plot(range(len(gaps)), gaps, c='g', lw=1, alpha=.9,
-            label='biological')
+    fig = plt.figure(figsize=(10, 4))
+    ax = fig.add_subplot(1, 2, 1)
+    ax_stationary = fig.add_subplot(1, 2, 2)
 
-    sim_gaps = estimate_gap_probs_in_opseq(sim_sample, radius)
-    ax.plot(range(len(sim_gaps)), sim_gaps, c='k', lw=1,
-            alpha=.9, label='simulated')
+    gap = .15
+    n_samples = 50
+    K = 1200
+    radius = 100
 
-    ax.axhline(y=gap_hat, c='k', alpha=.2, lw=3)
-    ax.grid(True)
-    ax.legend(fontsize=11)
-    ax.set_xlim(radius, sample_len - radius)
-    ax.set_xlabel('position in edit sequence')
-    ax.set_ylabel('estimated gap probability ($r=%d$)' % radius)
-    ax.set_ylim(-.1, 1)
+    for mode, ax in zip(['stationary', 'original'], [ax_stationary, ax]):
+        opseq = full_opseq
+        if mode == 'stationary':
+            opseq = re.sub('D{10,}', 'D', opseq)
+            opseq = re.sub('I{10,}', 'I', opseq)
+
+        real_samples = sample_opseq(opseq, K, n_samples, gap)
+        real_labeled = False
+        for real_sample in real_samples:
+            label = 'biological' if not real_labeled else None
+            real_gaps = gaussian_filter1d(
+                estimate_gap_probs_in_opseq(real_sample, radius), radius/5)
+            ax.plot(range(K), real_gaps, c='g', lw=1, alpha=.2, label=label)
+            real_labeled = True
+
+        sim_labeled = False
+        for _ in range(n_samples):
+            sim_sample = ''.join(
+                np.random.choice(
+                    list('MID'), p=[1 - gap, gap / 2, gap / 2], size=K
+                )
+            )
+            label = 'simulated' if not sim_labeled else None
+            sim_gaps = gaussian_filter1d(
+                estimate_gap_probs_in_opseq(sim_sample, radius), radius/5)
+            ax.plot(range(K), sim_gaps, c='k', lw=1, alpha=.4, label=label)
+            sim_labeled = True
+
+        ax.axhline(y=gap, c='k', alpha=.2, lw=3)
+        ax.grid(True)
+        ax.legend(loc='upper right', fontsize=8)
+        ax.set_xlim(radius, K - radius)
+        ax.set_xlabel('position in edit sequence')
+        ax.set_ylabel('gap probability')
+        ax.set_ylim(-.05, .6)
+
     savefig(fig, 'gap probabilities.png')
 
 
